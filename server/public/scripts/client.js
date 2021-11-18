887463d79e58aaf6e6d264eb3862d67330bde6d9
Bennett Smrdel <smrdelb@gmail.com>
2021-11-18 09:50:54 -0600

    added click event listener for delete button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,9 @@ $( document ).ready( function(){
   // load existing koalas on page load
   getKoalas();
 
+  // Adds a click listener to detect when delete button is clicked.
+    // TODO make sure to add class delete-btn, to the remove button.
+  $('body').on('click', '.delete-btn', deleteKoala);
 }); // end doc ready
 
 function setupClickListeners() {
@@ -37,4 +40,19 @@ function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  
+}
+
+function deleteKoala( newKoala ){
+  console.log( 'in deleteKoala');
+  const koalaId = $(this).data('id');
+  // ajax call to server to delete koalas
+  $.ajax({
+    type: 'DELETE',
+    url: `/koalas/${koalaId}`
+  }).then(function(response) {
+    console.log('response', response);
+    // TODO refreshKoalas();
+  }).catch(function(error){
+    console.log('error: ', error);
+  });
 }
