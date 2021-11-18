9ed2d7c0b8a91d80e1b8074a18db04875c7c8a5e
whoguy24 <whoguy24@gmail.com>
2021-11-18 10:28:51 -0600

    Wrote newKoala function.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,28 +3,23 @@ console.log( 'js' );
 $( document ).ready( function(){
   console.log( 'JQ' );
   // Establish Click Listeners
-  setupClickListeners()
+  setupClickListeners();
   // load existing koalas on page load
   getKoalas();
 
+  // Adds a click listener to detect when delete button is clicked.
+  // TODO make sure to add class delete-btn, to the remove button.
+  $('#addButton').on('click', saveKoala)
+  $('body').on('click', '.delete-btn', deleteKoala);
 }); // end doc ready
 
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+// clear koalaInputs
+function clearInputs(){
+  $('#nameIn').val('');
+  $('#ageIn').val('');
+  $('#genderIn').val('');
+  $('#readyForTransferIn').val('');
+  $('#notesIn').val('');
 }
 
 function getKoalas(){
@@ -52,4 +47,19 @@ function saveKoala(){
     getKoalas();
   });
 
+}
+
+function deleteKoala( newKoala ){
+  console.log( 'in deleteKoala');
+  const koalaId = $(this).data('id');
+  // ajax call to server to delete koalas
+  $.ajax({
+    type: 'DELETE',
+    url: `/koalas/${koalaId}`
+  }).then(function(response) {
+    console.log('response', response);
+    // TODO refreshKoalas(); 
+  }).catch(function(error){
+    console.log('error: ', error);
+  });
 }
