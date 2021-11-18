19ba6f8ff0326f4e6fad6df635a86543b858dade
Bennett Smrdel <smrdelb@gmail.com>
2021-11-18 10:08:45 -0600

    created router.delete

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -14,5 +14,26 @@ const koalaRouter = express.Router();
 
 
 // DELETE
+// Bennet was here
+router.delete('/:id', (req, res) => {
+    console.log('DELETE /koala/:id');
+    console.log('req.params:', req.params);
+    const koalaId = req.params.id;
+    const sqlText = `
+      DELETE FROM "koalas"
+        WHERE "id"=$1;
+    `;
+    const sqlValues = [ koalaId ];
+  
+    pool.query(sqlText, sqlValues)
+      .then((dbResult) => {
+        res.sendStatus(200);
+      })
+      .catch((dbErr) => {
+        console.error(dbErr);
+        res.sendStatus(500);
+      })
+  });
+
 
 module.exports = koalaRouter;
